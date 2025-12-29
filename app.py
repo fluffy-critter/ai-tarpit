06ae9bb86a5cf1a34eaf6f7a6b95f6cfefec53e8
fluffy <fluffy@beesbuzz.biz>
2025-12-29 12:08:23 -0800

    Add some artificial delay

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """ main implementation of the thing """
 
+import asyncio
 import random
 import string
 
@@ -114,6 +115,7 @@ async def app(scope, _, send):
         <body><p>Hello</p></body></html>'''.encode('utf-8')
     else:
         content_type = b'text/html; charset=utf-8'
+        await asyncio.sleep(3)
         body = make_page().encode('utf-8')
 
     await send({
